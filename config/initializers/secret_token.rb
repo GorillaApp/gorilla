63b1f2ccc8f7124b0e9031e3d385fedfb5e38787
Zachary Bush <zmbush@berkeley.edu>
2013-02-28 17:20:33 -0800

    Initial Gorilla Setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gorilla::Application.config.secret_token = '61e30b588c24f4bfbdcaa4cb43162ad4d7965eee15d8f3488adbc3c02b55931c39a96279158ea20658324c619f73db9996ebd367b8957af10acaa47eb55b18b5'
