2ed00850c4ce3d6e85a09a8fee4d1b9cd55f12bb
Zachary Bush <zmbush@berkeley.edu>
2013-03-01 17:42:47 -0800

    after migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20130302013728) do
+
+  create_table "autosaves", :force => true do |t|
+    t.string   "name"
+    t.datetime "save_date"
+    t.integer  "user_id"
+    t.text     "contents"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
 end
