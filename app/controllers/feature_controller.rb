4cffe9a5215d02b5ca66430196383b77b4ec6344
Eric <eric.d.wishart@gmail.com>
2013-03-28 14:24:56 -0700

    Added features.rb

---
+++ app/controllers/feature_controller.rb
@@ -0,0 +1,21 @@
+class FeatureController < ApplicationController
+
+	def add
+		result = Features.add(params)
+	end
+
+	def remove
+		result = Features.remove(params)
+	end
+
+	def edit
+		result = Features.
+	end
+
+	def getAll
+	end
+
+	def getFeature
+	end
+
+end
