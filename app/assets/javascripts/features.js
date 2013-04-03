78c5b93f274f9905053845d287dbe8ea24b9a9c0
Eric <eric.d.wishart@gmail.com>
2013-04-03 15:04:54 -0700

    Fixing allFeatures

--- app/assets/javascripts/features.js
+++ app/assets/javascripts/features.js
@@ -1,3 +1,25 @@
+function populateTbl(){ 
+  var table=document.getElementById("featureTbl");
+  for (var i = 0; i < allFeatures.length; i++) {
+    var feat = allFeatures[i];
+
+    var row=table.insertRow(-1);
+
+    var cell1=row.insertCell(0);
+    var cell2=row.insertCell(1);
+    var cell3=row.insertCell(2);
+    var cell4=row.insertCell(3);
+    var cell4=row.insertCell(4);
+
+    cell1.innerHTML = feat.id;
+    cell2.innerHTML = feat.name;
+    cell3.innerHTML = feat.sequence;
+    cell4.innerHTML = feat.forward_color;
+    cell5.innerHTML = feat.reverse_color;   
+    cell6.innerHTML = "<a href='/feature/delete?id="+feat.id+"&user_id="+user_id+"'>Delete Feature</a>";
+  }
+}
+
 $(function() {
     $( "#featuredialog" ).dialog({
       autoOpen: false,
@@ -35,31 +57,15 @@ $(function() {
           'GET',
           "/feature/getAll",
           {user_id: user},
-          function(data) { allFeatures = data; },
+          function(data) { allFeatures = data;
+                           populateTbl(); },
           function(err) {   }
         );
+      } else {
+        populateTbl();
       }
-
-      var table=document.getElementById("featureTbl");
-      for (var i = 0; i < allFeatures.length; i++) {
-        var feat = allFeatures[i];
-
-        var row=table.insertRow(-1);
-
-        var cell1=row.insertCell(0);
-        var cell2=row.insertCell(1);
-        var cell3=row.insertCell(2);
-        var cell4=row.insertCell(3);
-        var cell4=row.insertCell(4);
-
-        cell1.innerHTML = feat.id;
-        cell2.innerHTML = feat.name;
-        cell3.innerHTML = feat.sequence;
-        cell4.innerHTML = feat.forward_color;
-        cell5.innerHTML = feat.reverse_color;   
-        cell6.innerHTML = "<a href='/feature/delete?id="+feat.id+"&user_id="+user_id+"'>Delete Feature</a>";
-      }    
 
       $( "#allfeaturesdialog" ).dialog( "open" );
     });
   });
+
