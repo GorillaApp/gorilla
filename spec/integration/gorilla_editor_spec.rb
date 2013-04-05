7dc5ccdfb260c169f353cf8f565ee4ea87c4496a
Zachary Bush <zmbush@berkeley.edu>
2013-04-05 01:51:50 -0700

    Fix gorilla_editor_spec

--- spec/integration/gorilla_editor_spec.rb
+++ spec/integration/gorilla_editor_spec.rb
@@ -7,7 +7,7 @@ describe 'GorillaEditor', :js => true do
     before (:each) do
       visit '/'
 
-      find("#user_nav").click_link "Sign up"
+      find("#users_form").click_link "Sign up"
 
       fill_in :user_email, with: "test@test.com"
       fill_in :user_password, with: "password"
@@ -15,13 +15,14 @@ describe 'GorillaEditor', :js => true do
 
       click_button "Sign up"
 
-      page.should have_content "Signed in as test@test.com"
+      page.should have_content "Signed in as: test@test.com"
+      page.should have_content "Welcome!"
     end
 
     it 'should load a simple file' do
       visit '/testclient/client'
 
-      fill_in :file, :with => <<-EOF
+      find('#file').set <<-EOF
 LOCUS pGG001 20 bp ds-DNA circular UNK 01-JAN-1980
 FEATURES             Location/Qualifiers
      misc_feature    complement(1..10)
@@ -34,11 +35,11 @@ ORIGIN
         1 cgtctctgac cagaccaata
 //
   EOF
-      click_button "Process"
+      click_button "Open File"
 
       page.should have_content "cgtctctgaccagaccaata"
 
-      find('#ColE1-0-0-ed').should have_content "cgtctctgac"
+      find('#ColE1-0-0-main_editor').should have_content "cgtctctgac"
     end
   end
 end
