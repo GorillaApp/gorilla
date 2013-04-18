64fd1554d004264ef04c90123a7e7fa4e5bf09f8
Chris Henzie <chrishenzie@gmail.com>
2013-04-18 14:57:35 -0700

    got rid of User.create, started using FactoryGirl.create(:user)

--- spec/requests/password_reset_spec.rb
+++ spec/requests/password_reset_spec.rb
@@ -5,20 +5,21 @@ describe "password reset" do
     describe "reset_password_token for users" do
 
         it "should be nil on new user creation" do
-          user = User.create(:email => "chris@chrishenzie.com", :password => "password")
+          user = FactoryGirl.create(:user)
           user.reset_password_token.should be_nil
         end
 
         it "should not be nil on sending of password reset instructions" do
-          user = User.create(:email => "chris@chrishenzie.com", :password => "password")
+          user = FactoryGirl.create(:user)
           user.send_reset_password_instructions
           user.reset_password_token.should_not be_nil
         end
 
         it "should be nil after password reset" do
-          user = User.create(:email => "chris@chrishenzie.com", :password => "password")
+          user = FactoryGirl.create(:user)
           user.send_reset_password_instructions
           user.reset_password!("new password", "new password")
+          user.reload
           user.reset_password_token.should be_nil
         end
 
@@ -27,16 +28,16 @@ describe "password reset" do
     describe "reset_password!() method" do
 
       it "should return false if passwords do not match" do
-        user = User.create(:email => "chris@chrishenzie.com", :password => "password")
+        user = FactoryGirl.create(:user)
         user.reset_password!("these", "dont match").should eql(false)
       end
 
       it "should return true if passwords match and successfully changes password" do
-        user = User.create(:email => "chris@chrishenzie.com", :password => "password")
+        user = FactoryGirl.create(:user)
         user.reset_password!("new password", "new password").should eql(true)
 
         visit "/users/sign_in"
-        fill_in "Email",            :with => "chris@chrishenzie.com"
+        fill_in "Email",            :with => user.email
         fill_in "user_password",    :with => "new password"
 
         click_button "Sign in"
@@ -68,16 +69,16 @@ describe "password reset" do
 
       it "should provide error if user does not exist" do
         visit "/users/password/new"
-        fill_in "Email",            :with => "chris@chrishenzie.com"
+        fill_in "Email",            :with => "user@doesnotexist.com"
         click_button "Reset my password"
 
         page.should have_content("Email not found")
       end
 
       it "should redirect to sign in page if user exists" do
-        user = User.create(:email => "chris@chrishenzie.com", :password => "password")
+        user = FactoryGirl.create(:user)
         visit "/users/password/new"
-        fill_in "Email",            :with => "chris@chrishenzie.com"
+        fill_in "Email",            :with => user.email
         click_button "Reset my password"
 
         page.should have_content("You will receive an email with instructions about how to reset your password in a few minutes.")
@@ -88,7 +89,7 @@ describe "password reset" do
     describe "password reset form" do
 
       it "should exist and be unique to the user" do
-        user = User.create(:email => "chris@chrishenzie.com", :password => "password")
+        user = FactoryGirl.create(:user)
         user.send_reset_password_instructions
         token = user.reset_password_token
         url = "/users/password/edit?reset_password_token=" + token
@@ -99,7 +100,7 @@ describe "password reset" do
       end
 
       it "should allow user to successfully reset their password" do
-        user = User.create(:email => "chris@chrishenzie.com", :password => "password")
+        user = FactoryGirl.create(:user)
         user.send_reset_password_instructions
         token = user.reset_password_token
         url = "/users/password/edit?reset_password_token=" + token
@@ -114,7 +115,7 @@ describe "password reset" do
       end
 
       it "user should be able to reset password, log out, and log in with new password" do
-        user = User.create(:email => "chris@chrishenzie.com", :password => "password")
+        user = FactoryGirl.create(:user)
         user.send_reset_password_instructions
         token = user.reset_password_token
         url = "/users/password/edit?reset_password_token=" + token
@@ -127,7 +128,7 @@ describe "password reset" do
         click_link "Sign out"
 
         visit "/users/sign_in"
-        fill_in "Email",            :with => "chris@chrishenzie.com"
+        fill_in "Email",            :with => user.email
         fill_in "user_password",    :with => "123456789"
         click_button "Sign in"
 
