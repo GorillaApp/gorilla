de55954c92787c557b0be727b7ed10ba163263a9
Erika Delk <etdelk@gmail.com>
2013-04-18 14:37:01 -0700

    finishing up the enzyme backend code

---
+++ app/controllers/enzyme_controller.rb
@@ -0,0 +1,40 @@
+class EnzymeController < ApplicationController
+
+  SUCCESS = 1
+  NAME_EXISTS = 2
+  SEQUENCE_EXISTS = 3
+  DOES_NOT_EXIST = 4
+  UNEXPECTED_EXCEPTION = 5
+
+  def add
+    #no matter what happens, the result is returned as an error code
+    result = Enzyme.add(params)
+    render :json => {:errCode => result}
+  end
+
+  def remove
+    result = Enzyme.remove(params)
+    render :json => {:errCode => result}
+  end
+
+  def edit
+    result = Enzyme.edit(params)
+    render :json => {:errCode => result}
+  end
+
+  def getAll
+    #returns an array of features
+    result = Enzyme.getAll(params)
+    render :json => {:features => result}
+  end
+
+  def getEnzyme
+    result = Enzyme.getFeature(params)
+    if result == DOES_NOT_EXIST
+      render :json => {:errCode => result}
+    else
+      render :json => {:features => result}
+    end
+  end
+
+end
--- spec/models/enzyme_spec.rb
+++ spec/models/enzyme_spec.rb
@@ -1,5 +1,52 @@
 require 'spec_helper'
 
 describe Enzyme do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    FactoryGirl.create(:enzyme).should be_valid
+  end
+
+  it "should be able to be added even when user doesn't exist"  do
+    params = FactoryGirl.attributes_for(:enzyme)
+    result = Enzyme.add(params)
+    result.should == 1
+  end
+
+  it "should return success when removing enzymes"  do
+    params = FactoryGirl.attributes_for(:enzyme)
+    Enzyme.add(params)
+    enz = Enzyme.find_by_name(params[:name])
+    result = Enzyme.remove({:id => enz.id})
+    result.should == 1
+  end
+
+  it "should be able to remove a enzyme from the database" do
+    params = FactoryGirl.attributes_for(:enzyme)
+    Enzyme.add(params)
+    enz = Enzyme.find_by_name(params[:name])
+    ID = enz.id
+    params2 = {:id => ID}
+    Enzyme.remove(params2)
+    result = Enzyme.find_by_name(params[:name])
+    result.should == nil
+  end
+
+  it "tests that edit always return success" do
+    params = FactoryGirl.attributes_for(:enzyme)
+    Enzyme.add(params)
+    enz = Enzyme.find_by_name(params[:name])
+    params2 = FactoryGirl.attributes_for(:feature2, :name => params[:name])
+    params2[:id] = enz.id
+    result = Enzyme.edit(params2)
+    result.should == 1
+  end
+
+  it "should return all enzymes associated with a user id" do
+    params = FactoryGirl.attributes_for(:enzyme)
+    params2 = FactoryGirl.attributes_for(:enzyme)
+    Enzyme.add(params)
+    Enzyme.add(params2)
+    params3 = {:user_id => 12345}
+    puts Enzyme.getAll(params3)
+  end
+
 end
