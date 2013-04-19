55890518276174f690ba4af526b97032f18c3dce
Chris Henzie <chrishenzie@gmail.com>
2013-04-19 07:01:36 -0700

    oh my god I've been writing the wrong var name this whole time. I'm so tired

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,14 +8,19 @@ class ApplicationController < ActionController::Base
   # # 	request.env['devise.skip_trackable'] = true
   # # end
 
-  def after_token_authentication
-  	if params[:auth_key].present?
-  		@user = User.find_by_authentication_token(params[:auth_key]) 
-  		if ! @user.nil?
-  			sign_in(:user, @user)
-  			redirect_to new_user_session_path
-  		end
-  	end
-  end
+    def after_token_authentication
+      #puts "fuck you" * 500
+      auth_token = params[:auth_token]
+      puts auth_token * 500
+      if params[:auth_key].present?
+        #puts "shit" * 500
+        @user = User.find_by_authentication_token(params[:auth_token]) 
+        if user
+          #puts "fuck you"
+          sign_in(:user, @user)
+          redirect_to new_user_session_path
+        end
+      end
+    end
 
 end
--- app/controllers/testclient_controller.rb
+++ app/controllers/testclient_controller.rb
@@ -1,16 +1,18 @@
 class TestclientController < ApplicationController
     #before_filter :authenticate_user!
     before_filter :after_token_authentication
-    def client
-    	#puts "fuck you" * 500
-    	if !params[:auth_key].blank?
-    		puts "shit"
-    		@user = User.find_by_authentication_token(params[:auth_key]) 
-    		if user
-    			#puts "fuck you"
-    			sign_in(:user, @user)
-    			redirect_to new_user_session_path
-    		end
-    	end
-    end
+    # def client
+    # 	#puts "fuck you" * 500
+    # 	auth_key = params[:auth_key]
+    # 	puts auth_key * 500
+    # 	if params[:auth_key].present?
+    # 		#puts "shit" * 500
+    # 		@user = User.find_by_authentication_token(params[:auth_key]) 
+    # 		if user
+    # 			#puts "fuck you"
+    # 			sign_in(:user, @user)
+    # 			redirect_to new_user_session_path
+    # 		end
+    # 	end
+    # end
 end
