f06daa26008114eef551d42a66fa1a14461c94da
Chris Henzie <chrishenzie@gmail.com>
2013-04-19 01:28:16 -0700

    users now have authentication tokens generated on account creation, changed test

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,8 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+
+  # Added so that authentication token is created on new user account
+  before_save :ensure_authentication_token
+
 end
--- spec/requests/authentication_token_spec.rb
+++ spec/requests/authentication_token_spec.rb
@@ -6,7 +6,7 @@ describe "authentication token" do
   describe "token" do
     it "should be nil on account creation" do
       user = FactoryGirl.create(:user)
-      user.authentication_token.should be_nil
+      user.authentication_token.should_not be_nil
     end
   end
 
