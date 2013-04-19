811d6cee7047b9fd619de1af694b8a9dfb64cdd0
Chris Henzie <chrishenzie@gmail.com>
2013-04-19 00:51:02 -0700

    rolled back db, added token_authenticatable to model and migrated db

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :token_authenticatable
 
   has_many :features
 
