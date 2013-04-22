ee7af5bdd52e062f6f052a89b4f523038a72ad91
Zachary Bush <zach@zmbush.com>
2013-04-22 16:57:19 -0700

    Fix migration to be reversible

--- db/migrate/20130422210202_change_sequence_to_text.rb
+++ db/migrate/20130422210202_change_sequence_to_text.rb
@@ -1,5 +1,13 @@
 class ChangeSequenceToText < ActiveRecord::Migration
-  def change
+  def up
     change_column :features, :sequence, :text
+  end
+
+  def down
+    Feature.where('LENGTH(sequence) > 255').each do |n|
+        n.sequence = n.sequence[0..254]
+        n.save!
+    end
+    change_column :features, :sequence, :string
   end
 end
