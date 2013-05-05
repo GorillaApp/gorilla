62b3eaece6b1ffea5e0308f0ffc15caf11a3c128
Zachary Bush <zach@zmbush.com>
2013-05-04 23:41:14 -0700

    Unminify mousetrap

--- vendor/assets/javascripts/mousetrap-global.js
+++ vendor/assets/javascripts/mousetrap-global.js
@@ -1,1 +1,35 @@
-Mousetrap=function(a){var d={},e=a.stopCallback;a.stopCallback=function(b,c,a){return d[a]?!1:e(b,c,a)};a.bindGlobal=function(b,c,e){a.bind(b,c,e);if(b instanceof Array)for(c=0;c<b.length;c++)d[b[c]]=!0;else d[b]=!0};return a}(Mousetrap);
+/**
+ * adds a bindGlobal method to Mousetrap that allows you to
+ * bind specific keyboard shortcuts that will still work
+ * inside a text input field
+ *
+ * usage:
+ * Mousetrap.bindGlobal('ctrl+s', _saveChanges);
+ */
+Mousetrap = (function(Mousetrap) {
+    var _global_callbacks = {},
+        _original_stop_callback = Mousetrap.stopCallback;
+ 
+    Mousetrap.stopCallback = function(e, element, combo) {
+        if (_global_callbacks[combo]) {
+            return false;
+        }
+ 
+        return _original_stop_callback(e, element, combo);
+    };
+ 
+    Mousetrap.bindGlobal = function(keys, callback, action) {
+        Mousetrap.bind(keys, callback, action);
+ 
+        if (keys instanceof Array) {
+            for (var i = 0; i < keys.length; i++) {
+                _global_callbacks[keys[i]] = true;
+            }
+            return;
+        }
+        
+        _global_callbacks[keys] = true;
+    };
+ 
+    return Mousetrap;
+}) (Mousetrap);
